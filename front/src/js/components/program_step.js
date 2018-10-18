c7f9feffcf3b4f22384fbddb7c32014f9720a654
Berenger Salmon <berenger.salmon@gmail.com>
2018-10-18 18:32:35 +0200

    Component - program-steps

--- front/src/js/components/program_step.js
+++ front/src/js/components/program_step.js
@@ -5,19 +5,18 @@ export default class ProgramStep extends React.Component{
     super(props)
   }
   render(){
-    const { name, image, repetitions, rest_duration_between_series, rest_end_duration, series, weight } = this.props.program_step
+    const { exercice, repetitions, rest_duration_between_series, rest_end_duration, series, weight } = this.props.program_step
 
     return (
       <div className='btn btn-light col mb-5'>
-        <h3 className="title-h3">{name}</h3>
-        <ul>
-          {image?<div><img className='item_steps' src={image}/></div>:''}
-          <div>Reps : {repetitions}</div>
-          <div>Rest duration between series : {rest_duration_between_series}s</div>
-          <div>Rest end duration : {rest_end_duration}s</div>
-          <div>Series : {series}</div>
-          <div>Weight : {weight}kg</div>
-        </ul>
+        <p className='btn btn-dark d-block float-none float-sm-right'>edit</p>
+        <span className="programStep-title text-truncate">{exercice.name}</span>
+        {exercice.image?<div><img className='item_steps' src={exercice.image}/></div>:''}
+        <div>Reps : {repetitions}</div>
+        <div>Rest duration between series : {rest_duration_between_series}s</div>
+        <div>Rest end duration : {rest_end_duration}s</div>
+        <div>Series : {series}</div>
+        <div>Weight : {weight}kg</div>
       </div>
     )
   }
