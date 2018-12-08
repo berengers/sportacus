4e5f04cb71afdd2e365999e384a12447d1ee4e50
Berenger Salmon <berenger.salmon@gmail.com>
2018-12-08 13:11:58 +0100

    Redirect to login without token

--- front/src/js/routes/index.js
+++ front/src/js/routes/index.js
@@ -12,8 +12,13 @@ import { logout } from '../actions/login'
 export const routesMap = {
   HOME: {
     path: '/',
-    thunk: async (dispatch) => {
-      dispatch(redirect({ type: type.PROGRAMS }))
+    thunk: (dispatch) => {
+      const token = localStorage.getItem("token")
+      if (token) {
+        dispatch(redirect({ type: type.PROGRAMS }))
+      } else {
+        dispatch(redirect({ type: type.LOGIN }))
+      }
     }
   },
   PROGRAMS: {
@@ -22,7 +27,7 @@ export const routesMap = {
   },
   PROGRAM: {
     path: '/programs/program/:program_id',
-    thunk: async (dispatch, getState) => {
+    thunk: (dispatch, getState) => {
       const { location: { payload: { program_id } }, programs, currentProgram } = getState()
       dispatch({ type: type.CHANGE_PAGE, payload: { page: 'Programs' } })
       dispatch({ type: type.INITIAL_CURRENT_STEP })
@@ -37,14 +42,14 @@ export const routesMap = {
   },
   RUN_PROGRAM: {
     path: '/programs/program/:program_id/run',
-    thunk: async (dispatch, getState) => {
+    thunk: (dispatch, getState) => {
       const { location: { payload: { program_id } }, programs } = getState()
       dispatch(fetchProgram(program_id))
     }
   },
   FORM_STEP: {
     path: '/programs/program/:program_id/step/:step_id',
-    thunk: async (dispatch, getState) => {
+    thunk: (dispatch, getState) => {
       const { location: { payload: {step_id} } } = getState()
       dispatch(fetchStep(step_id))
     }
@@ -55,21 +60,21 @@ export const routesMap = {
   },
   NEW_STEP: {
     path: '/programs/program/:program_id/exercise/:exercise_id',
-    thunk: async (dispatch, getState) => {
+    thunk: (dispatch, getState) => {
       const { location: { payload: {exercise_id} } } = getState()
       dispatch(fetchExercise(exercise_id))
     }
   },
   EXERCISES: {
     path: '/exercises',
-    thunk: async (dispatch) => {
+    thunk: (dispatch) => {
       dispatch({ type: type.INITIAL_CURRENT_EXERCISE })
       dispatch(fetchExercises())
     }
   },
   EXERCISE: {
     path: '/exercises/exercise/:id',
-    thunk: async (dispatch, getState) => {
+    thunk: (dispatch, getState) => {
       const { location: { payload: { id } }, exercises } = getState()
       const exercise = (exercises.filter(exercise => exercise.id == id))[0]
       if (exercises.length > 0) {
@@ -84,13 +89,13 @@ export const routesMap = {
   USER: '/user/:id',
   LOGIN: {
     path: '/login',
-    thunk: async (dispatch) => {
+    thunk: (dispatch) => {
       dispatch({ type: type.CHANGE_PAGE, payload: { page: 'Login' } })
     }
   },
   LOGOUT: {
     path: '/logout',
-    thunk: async (dispatch) => {
+    thunk: (dispatch) => {
       dispatch(logout())
       dispatch(redirect({ type: type.LOGIN }))
     }
