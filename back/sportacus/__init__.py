5145142672d725936139d34d60f4bcfdc2f55d3d
Berenger Salmon <berenger.salmon@gmail.com>
2018-12-09 14:47:32 +0100

    Configuration Env Variables

--- back/sportacus/__init__.py
+++ back/sportacus/__init__.py
@@ -2,17 +2,18 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
+from flask_env import MetaFlaskEnv
+
+class Configuration(metaclass=MetaFlaskEnv):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres@localhost'
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 app = Flask(__name__)
 CORS(app)
 
-print ("NAME APP ---------------> ", Flask(__name__))
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres@localhost'
-app.config['SQLALCHEMY_ECHO'] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['ENV'] = 'development'
-app.config['DEBUG'] = True
+app.config.from_object(Configuration)
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
